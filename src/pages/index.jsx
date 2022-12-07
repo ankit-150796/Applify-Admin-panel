edb19bfae3482e640d3338f7a0772287b7477b63
ankit-150796 <ankit.s@applify.co>
2022-12-07 18:30:00 +0530

    Applify admin panel

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export { default as Dashboard } from './Dashboard';
+export { default as UserManagement } from './UserManagement';
+export { default as Admin } from './Admin';
+export { default as Notification } from './Notification';
+export { default as SystemConfiguration } from './SystemConfiguration';
+export { default as Report } from './Report';
+
+
+
